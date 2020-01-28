ecc59053aa6a6fb2cb8e973c838465ef21452030
Ionut Cristian <bistriceanu.ionut@gmail.com>
2020-01-28 12:47:40 +0200

    Update Program.cs

--- LockItDFS/LockItDFS/Program.cs
+++ LockItDFS/LockItDFS/Program.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace LockItDFS
@@ -11,15 +12,15 @@ namespace LockItDFS
             var matrix = new List<List<int>>()
             {
                 new List<int>() {1, 0, 0, 1, 0},
-                new List<int>() {1, 0, 1, 1, 0},
-                new List<int>() {1, 0, 0, 0, 0}
+                new List<int>() {0, 1, 1, 1, 0},
+                new List<int>() {0, 0, 0, 0, 0}
             };
 
             var regions = GetAllRegions(matrix);
-
             foreach (var region in regions)
             {
                 Console.WriteLine(region.ToString());
+                Console.WriteLine(region.IsValid);
             }
         }
 
@@ -45,11 +46,11 @@ namespace LockItDFS
             List<Region> totalRegions)
         {
             var region = new Region();
-            GetRegionSize(matrix, row, column, region);
+            PopulateRegionData(matrix, row, column, region);
             totalRegions.Add(region);
         }
 
-        private static int GetRegionSize(IReadOnlyList<List<int>> matrix, in int row, in int column, Region region)
+        private static int PopulateRegionData(IReadOnlyList<List<int>> matrix, in int row, in int column, Region region)
         {
             if (row < 0 || column < 0 || row >= matrix.Count || column >= matrix[row].Count)
             {
@@ -65,19 +66,52 @@ namespace LockItDFS
             var size = 1;
             region.AddIconPosition(new IconPosition(row, column));
 
+            AddIconToColumnDictionary(column, region);
+            AddIconToRowDictionary(row, region);
+
             for (var r = row - 1; r <= row + 1; r++)
             {
                 for (var c = column - 1; c <= column + 1; c++)
                 {
-                    if (r != row || c != column)
-                    {
-                        size += GetRegionSize(matrix, r, c, region);
-                    }
+                    if (r == row && c == column || CheckIfDiagonal(r, c, row, column))
+                        continue;
+
+                    size += PopulateRegionData(matrix, r, c, region);
                 }
             }
 
             region.Size = size;
             return size;
+        }
+
+        private static bool CheckIfDiagonal(int r, int c, in int row, in int column)
+        {
+            return ((r == row - 1 && c == column - 1) || (r == row - 1 && c == column + 1) ||
+                    (r == row && c == column + 1) || (r == row + 1 && c == column + 1));
+        }
+
+        private static void AddIconToRowDictionary(int rowKey, Region region)
+        {
+            if (!region.IconsCountByRow.ContainsKey(rowKey))
+            {
+                region.IconsCountByRow.Add(rowKey, 1);
+            }
+            else
+            {
+                region.IconsCountByRow[rowKey] = ++region.IconsCountByRow[rowKey];
+            }
+        }
+
+        private static void AddIconToColumnDictionary(int columnKey, Region region)
+        {
+            if (!region.IconsCountByColumn.ContainsKey(columnKey))
+            {
+                region.IconsCountByColumn.Add(columnKey, 1);
+            }
+            else
+            {
+                region.IconsCountByColumn[columnKey] = ++region.IconsCountByColumn[columnKey];
+            }
         }
     }
 
@@ -85,15 +119,30 @@ namespace LockItDFS
     {
         public int Size { get; set; } = 0;
         private List<IconPosition> _positions;
+        public Dictionary<int, int> IconsCountByRow;
+        public Dictionary<int, int> IconsCountByColumn;
+        public bool IsValid => IsValidRegion();
 
         public Region()
         {
             _positions = new List<IconPosition>();
+            IconsCountByRow = new Dictionary<int, int>();
+            IconsCountByColumn = new Dictionary<int, int>();
         }
 
         public void AddIconPosition(IconPosition iconPosition)
         {
             _positions.Add(iconPosition);
+        }
+
+        public IReadOnlyCollection<IconPosition> GetIconPositions()
+        {
+            return _positions;
+        }
+
+        private bool IsValidRegion()
+        {
+            return IconsCountByColumn.Any(icc => icc.Value == 3) || IconsCountByRow.Any(icr => icr.Value == 3);
         }
 
         public override string ToString()
@@ -111,18 +160,18 @@ namespace LockItDFS
 
     public class IconPosition
     {
-        private readonly int _row;
-        private readonly int _column;
+        public int Row { get; }
+        public int Column { get; }
 
         public IconPosition(int row, int column)
         {
-            _row = row;
-            _column = column;
+            Row = row;
+            Column = column;
         }
 
         public string GetPosition()
         {
-            return $"Icon is on row {_row} and column {_column}";
+            return $"Icon is on row {Row} and column {Column}";
         }
     }
 }
